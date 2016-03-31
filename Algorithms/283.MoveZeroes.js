cfa3d34cfa8b89285817a55dae62d192510da8d4
HandsomeOne <anthony_nju@foxmail.com>
2016-03-31 22:52:36 +0800

    improved #283 in a simpler way

--- Algorithms/283.MoveZeroes.js
+++ Algorithms/283.MoveZeroes.js
@@ -3,11 +3,10 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    for (var i = 0, j = 0, l = nums.length; i < l; i++ , j++) {
-        if (nums[j] === 0) {
-            nums.splice(j, 1);
+    for (var i = nums.length - 1; i >= 0; i--) {
+        if (nums[i] === 0) {
+            nums.splice(i, 1);
             nums.push(0);
-            j--;
         }
     }
 };
