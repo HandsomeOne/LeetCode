b56159db8484dc002c821b1454e2c45476df4606
HandsomeOne <anthony_nju@foxmail.com>
2016-03-31 20:40:39 +0800

    solved problem #18

--- Algorithms/18.4Sum.js
+++ Algorithms/18.4Sum.js
@@ -15,17 +15,17 @@ var fourSum = function(nums, target) {
         return a - b;
     });
     var l = nums.length;
-    for (var i = 0; i < l; i++) {
-        for (var j = i + (count[nums[i]] < 2); j < l; j++) {
-            for (var k = j + (count[nums[j]] < 3); k < l; k++) {
-                for (var m = k + (count[nums[k]] < 4); m < l; m++) {
-                    if (nums[i] + nums[j] + nums[k] + nums[m] === target) {
-                        quadruplets.push([nums[i], nums[j], nums[k], nums[m]]);
-                    }
+    for (var i = 0; nums[i] <= target / 4; i++) {
+        for (var j = i + (count[nums[i]] < 2),
+            jMax = (target - nums[i]) / 3; nums[j] <= jMax; j++) {
+            for (var k = j + (count[nums[j]] < 2 + (i === j)),
+                kMax = (target - nums[i] - nums[j]) / 2; nums[k] <= kMax; k++) {
+                var theLast = target - nums[i] - nums[j] - nums[k];
+                if (count[theLast] > (theLast === nums[i]) + (theLast === nums[j]) + (theLast === nums[k])) {
+                    quadruplets.push([nums[i], nums[j], nums[k], theLast]);
                 }
             }
         }
     }
     return quadruplets;
 };
-fourSum([-494, -474, -425, -424, -391, -371, -365, -351, -345, -304, -292, -289, -283, -256, -236, -236, -236, -226, -225, -223, -217, -185, -174, -163, -157, -148, -145, -130, -103, -84, -71, -67, -55, -16, -13, -11, 1, 19, 28, 28, 43, 48, 49, 53, 78, 79, 91, 99, 115, 122, 132, 154, 176, 180, 185, 185, 206, 207, 272, 274, 316, 321, 327, 327, 346, 380, 386, 391, 400, 404, 424, 432, 440, 463, 465, 466, 475, 486, 492], -1211);
