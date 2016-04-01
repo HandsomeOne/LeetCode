f4259c7329418045cce14f5bdbcd637a10c36430
handsomeOne <anthony_nju@foxmail.com>
2016-04-01 10:10:39 +0800

    `s1 += s2` is better than `s1 = s1.concat(s2)1

--- Algorithms/6.ZigZagConversion.js
+++ Algorithms/6.ZigZagConversion.js
@@ -12,16 +12,16 @@ var convert = function(s, numRows) {
     for (var i = 0; i < numRows; i++) {
         if (i === 0) {
             for (var j = 0; 2 * j * (numRows - 1) < l; j++) {
-                converted = converted.concat(s[2 * j * (numRows - 1)]);
+                converted += s[2 * j * (numRows - 1)];
             }
         } else if (i === numRows - 1) {
             for (var j = 0; (2 * j + 1) * (numRows - 1) < l; j++) {
-                converted = converted.concat(s[(2 * j + 1) * (numRows - 1)]);
+                converted += s[(2 * j + 1) * (numRows - 1)];
             }
         } else {
             for (var j = 0; 2 * j * (numRows - 1) < l; j++) {
-                converted = converted.concat(s[2 * j * (numRows - 1) + i] || '');
-                converted = converted.concat(s[2 * (j + 1) * (numRows - 1) - i] || '');
+                converted += s[2 * j * (numRows - 1) + i] || '';
+                converted += s[2 * (j + 1) * (numRows - 1) - i] || '';
             }
         }
     }
