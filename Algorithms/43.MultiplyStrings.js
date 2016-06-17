7a4241a2b253194c69222a283850338382670472
handsomeOne <i@handsomeone.com>
2016-06-17 11:24:02 +0800

    Enhanced. From now on, won't use `var`.

--- Algorithms/43.MultiplyStrings.js
+++ Algorithms/43.MultiplyStrings.js
@@ -7,43 +7,32 @@ var multiply = function(num1, num2) {
     if (num1 === '0' || num2 === '0') {
         return '0';
     }
-    var cache = [''];
-    var a = [];
-    for (var i = num2.length - 1; i >= 0; i--) {
-        var d = num2[i];
-        if (!(d in cache)) {
-            cache[d] = multiplyByDigit(num1, d);
-        }
-        a.push(cache[d]);
+    const splittedNum1 = [], splittedNum2 = [], p = [];
+    for (let i = num1.length; i > 0; i -= 4) {
+        splittedNum1.push(+num1.substring(i - 4, i));
     }
-
-    var carry = 0, result = '', l = a.length;
-    var m = a[l - 1].length + l;
-    for (var j = 1; j < m; j++) {
-        var t = 0;
-        for (var k = 0; k < j && k < l; k++) {
-            t += (+a[k][a[k].length - j + k] || 0);
-        }
-        t += carry;
-        result = (t % 10) + result;
-        carry = t / 10 | 0;
+    for (let i = num2.length; i > 0; i -= 4) {
+        splittedNum2.push(+num2.substring(i - 4, i));
+    }
+    splittedNum1.forEach((m, i) => {
+        splittedNum2.forEach((n, j) => {
+            if (i + j in p) {
+                p[i + j].push(m * n);
+            } else {
+                p[i + j] = [m * n];
+            }
+        });
+    });
+    let result = '', carry = 0;
+    for (let a of p) {
+        let sum = a.reduce((x, y) => x + y, 0) + carry;
+        let indice = '' + sum % 10000;
+        indice = '0'.repeat(4 - indice.length) + indice;
+        result = indice + result;
+        carry = sum / 10000 | 0;
     }
     if (carry) {
         result = carry + result;
     }
-    return result;
-
-    function multiplyByDigit(num, d) {
-        var carry = 0, result = '';
-        for (var i = num.length - 1; i >= 0; i--) {
-            var n = num[i];
-            var t = d * n + carry;
-            result = (t % 10) + result;
-            carry = t / 10 | 0;
-        }
-        if (carry) {
-            result = carry + result;
-        }
-        return result;
-    }
+    return result.replace(/^0+/, '');
 };
